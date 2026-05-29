8d64fb787729dc96711a018225839afb67463b0b
ShivrajKale1223 <shivraj.kale.1223@gmail.com>
2026-05-29 20:15:16 +0530

    Change app.run to use host and port settings

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def students():
     return render_template('students.html', students=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
